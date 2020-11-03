b1396cc1a8e1be1e7a0b7c19da82f9fd6de1eede
audrey-audrey <audreyuu@gmail.com>
2020-11-03 19:31:13 +0000

    Spinner II

---
+++ spinner2.js
@@ -0,0 +1,13 @@
+const loop = 5;
+
+for (let i = 0; i < loop; i++) {
+  setTimeout(() => process.stdout.write('\r|   '), 100 + (i * 800));
+  setTimeout(() => process.stdout.write('\r/   '), 300 + (i * 800));
+  setTimeout(() => process.stdout.write('\r-   '), 500 + (i * 800));
+  setTimeout(() => process.stdout.write('\r\\   '), 700 + (i * 800));
+}
+
+// print new line after loop
+setTimeout(() => /* New line */ process.stdout.write('\n'), 700 + (loop - 1) * 800);
+
+// Note to self: try putting the characters in an array
